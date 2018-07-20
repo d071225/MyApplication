5dd1df861e7f45f33fcdd5cec809d48807642803
d071225 <764552635@qq.com>
2018-07-20 09:52:14 +0800

    增加线程打印3

--- app/src/main/java/com/dyy/newtest/ui/activity/RetrofitActivity.java
+++ app/src/main/java/com/dyy/newtest/ui/activity/RetrofitActivity.java
@@ -38,6 +38,7 @@ public class RetrofitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_retrofit);
         ButterKnife.bind(this);
         Log.e(TAG,"onCreate---"+Thread.currentThread().getId());
+        Log.e(TAG,"onCreate--- hello3");
     }
 
     @OnClick(R.id.btn_req)
